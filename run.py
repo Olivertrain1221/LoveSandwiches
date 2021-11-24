c5b39dfb3fbc9791be3eda017c2ac1e366ab327f
Olivertrain1221 <olivertrain1996@gmail.com>
2021-11-24 20:58:35 +0000

    Add while loop to game to loop if the condition is not met for valid data input

--- run.py
+++ run.py
@@ -16,15 +16,19 @@ def get_sales_data():
     """
     Get sales figures input from the user
     """
-    print("Please enter sales data from the last market.")
-    print("Data shopuld be six numbers, separated by commas.")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data shopuld be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
 
-    data_str = input("Enter your data here: ")
-    
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Enter your data here: ")
+        
+        sales_data = data_str.split(",")
+        
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
 
 
 def validate_data(values):
@@ -33,16 +37,17 @@ def validate_data(values):
     Raises ValueError if strings cannot be converted into int,
     or if there arent exactly 6 values.
     """
+    print(values)
     try:
         if len(values) !=6:
-            raise ValueError(
+          raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
-
-
-
+        return False
+    
+    return True
 
 get_sales_data()
 
