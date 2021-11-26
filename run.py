46ed8feb3163536b6ade5bf5ffe91eb6226262cd
Olivertrain1221 <olivertrain1996@gmail.com>
2021-11-26 15:40:40 +0000

    Test to link Heroku

--- run.py
+++ run.py
@@ -1,5 +1,8 @@
 import gspread
 from google.oauth2.service_account import Credentials
+"""
+Test
+"""
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -7,82 +10,63 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file("creds.json")
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open("love_sandwiches")
+SHEET = GSPREAD_CLIENT.open('love_sandwiches')
+
 
 def get_sales_data():
     """
-    Get sales figures input from the user
+    Get sales figures input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of 6 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
         print("Please enter sales data from the last market.")
-        print("Data shopuld be six numbers, separated by commas.")
+        print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
+        data_str = input("Enter your data here:\n")
 
-        data_str = input("Enter your data here: ")
-        
         sales_data = data_str.split(",")
-        
+
         if validate_data(sales_data):
             print("Data is valid!")
             break
 
     return sales_data
-    
+
 
 def validate_data(values):
     """
-    Inside the try, converts all string values into intergers.
+    Inside the try, converts all string values into integers.
     Raises ValueError if strings cannot be converted into int,
-    or if there arent exactly 6 values.
+    or if there aren't exactly 6 values.
     """
-    print(values)
     try:
         [int(value) for value in values]
-        if len(values) !=6:
-          raise ValueError(
+        if len(values) != 6:
+            raise ValueError(
                 f"Exactly 6 values required, you provided {len(values)}"
             )
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
-    
+
     return True
-
-# def update_sales_worksheet(data):
-#     """
-#     Updates the sales worksheet in google drive, add new row with the list data provided.
-#     """
-#     print("Updating sales worksheet now....\n")
-#     sales_worksheet = SHEET.worksheet("sales")
-#     sales_worksheet.append_row(data)
-#     print("Sales worksheet updated successfully.\n")
-
-# def update_surplus_data_sheet(data):
-#     """
-#     Update the excel spreadsheet in surplus data
-#     """
-#     print("Updating surplus data sheet.....")
-#     surplus_worksheet = SHEET.worksheet("surplus")
-#     surplus_worksheet.append_row(data)
-#     print("Updated surplus")
 
 
 def update_worksheet(data, worksheet):
     """
-    compare sales with stock and calculate the surplus for each item type.
-
-    The surplus is defined as the sales figure subtracted form the stock:
-    -Positive surplus indicates waste
-    -Negative surplus indicates extra made when stock was sold out
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
     """
     print(f"Updating {worksheet} worksheet...\n")
     worksheet_to_update = SHEET.worksheet(worksheet)
     worksheet_to_update.append_row(data)
-    print(f"{worksheet} worksheet updates successfully\n")
+    print(f"{worksheet} worksheet updated successfully\n")
 
 
 def calculate_surplus_data(sales_row):
@@ -96,35 +80,36 @@ def calculate_surplus_data(sales_row):
     stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
     
-
     surplus_data = []
     for stock, sales in zip(stock_row, sales_row):
         surplus = int(stock) - sales
         surplus_data.append(surplus)
-    
+
     return surplus_data
+
 
 def get_last_5_entries_sales():
     """
-    Collects collumns of data from sales worksheet, collecting
+    Collects columns of data from sales worksheet, collecting
     the last 5 entries for each sandwich and returns the data
     as a list of lists.
     """
     sales = SHEET.worksheet("sales")
-    
+
     columns = []
-    for individual_value in range(1, 7):
-        column = sales.col_values(individual_value)
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
         columns.append(column[-5:])
-    
+
     return columns
 
-def calculate_stock_data( ):
+
+def calculate_stock_data(data):
     """
-    Calculate the average stock for each item, adding 10%
+    Calculate the average stock for each item type, adding 10%
     """
-    print("Calculating stock data..\n")
-    new_stock_data= []
+    print("Calculating stock data...\n")
+    new_stock_data = []
 
     for column in data:
         int_column = [int(num) for num in column]
@@ -135,19 +120,19 @@ def calculate_stock_data( ):
     return new_stock_data
 
 
-
 def main():
     """
-    Run all program functions:
+    Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_worksheet(sales_data, "sales") 
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_worksheet(sales_data, "surplus") 
+    update_worksheet(new_surplus_data, "surplus")
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
-    print(stock_data)
+    update_worksheet(stock_data, "stock")
+
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
